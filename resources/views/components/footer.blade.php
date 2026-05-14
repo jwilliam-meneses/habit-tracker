20f10ec4b177f3301618957ebc6a95aa6a3fe9a0
William Meneses <jwilliam.anjos@gmail.com>
2026-05-14 00:04:33 -0300

    feat: init component header/footer/layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-white border-t-2 p-4">
+    <p class="text-center">
+        Criado por William. O código fonte está no <a href="#" class="underline">Github</a>.
+    </p>
+</footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,9 @@
+<header  class="bg-white border-b-2 flex items-center justify-between p-4">
+   <div>
+        Logo
+   </div>
+
+    <div>
+        github
+   </div>
+</header>
